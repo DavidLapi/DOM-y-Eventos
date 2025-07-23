94a182c6a3c2fb767217867d4c6b5aab2bbc5a3c
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-07-23 11:17:40 +0200

    En desarrollo 23/07

---
+++ script.js
@@ -0,0 +1,25 @@
+//Ejercicio 1
+
+const textoInicial = document.getElementById("texto-inicial");
+textoInicial.textContent = "Me llamo David y tengo 26 años";
+
+//Ejercicio 2
+
+const textoColor = document.getElementById("texto-color");
+textoColor.style.color = "red";
+
+//Ejercicio 3
+
+//Ejercicio 4
+
+//Ejercicio 5
+
+//Ejercicio 6
+
+//Ejercicio 7
+
+//Ejercicio 8
+
+//Ejercicio 9
+
+//Ejercicio 10
